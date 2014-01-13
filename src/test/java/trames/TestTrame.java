158250d6001fa215089f231d6f1929b00113f8b2
Quentin Dunoyer <quentin.dunoyer@insa-lyon.fr>
2014-01-13 11:54:17 +0100

    TestTrame : Ajout de TestTrame

---
+++ src/test/java/trames/TestTrame.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package trames;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author qdunoyer
+ */
+public class TestTrame {
+    
+    public TestTrame() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void testCasUsuel(){
+        Trame test = new Trame("A55A0B07B729000D00053E7B00BD");
+        assertEquals("00053E7B",test.getID());
+        assertEquals(0xB729000D,test.getData());
+    }
+}
