17880eab77f9992ada9fe82b1ba7c813bce25523
Jérémy <Jrmy@134.214.220.235>
2014-02-17 09:48:16 +0100

    ajout de méthodes pour encrypter et décrypter une string, et un test

--- src/main/java/h4414/ghome/camel/processors/Security.java
+++ src/main/java/h4414/ghome/camel/processors/Security.java
@@ -6,6 +6,8 @@
 
 package h4414.ghome.camel.processors;
 
+import java.security.*;
+import javax.crypto.*;
 import org.apache.camel.Message;
 
 /**
@@ -30,4 +32,53 @@ public class Security {
         
     }
     
+    public static SecretKey GenererCle(){
+        try{
+            KeyGenerator keyGen = KeyGenerator.getInstance("DES");
+            keyGen.init(56);
+            SecretKey key = keyGen.generateKey(); 
+            return key;
+        } catch (NoSuchAlgorithmException e){
+            return null;
+        }     
+    }
+    
+    public static byte[] Crypter(String text, Key key){
+        try{
+            System.out.println("Debut cryptage");
+            //Generer un objet Cipher
+            Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+            //Encrypter en utilisant la clé
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] plainText = text.getBytes();
+            byte[] cipherText = cipher.doFinal(plainText);
+            String result = new String(cipherText, "ISO-8859-1");
+            System.out.println("Message crypté : ");
+            System.out.println(result);
+            System.out.println("Fin cryptage");
+            return cipherText;            
+        }catch(Exception e){
+            System.out.println("Erreur cryptage");
+            return null;
+        }
+    }
+    
+    public static String Decrypter(byte[] crypText, Key key){
+        try{
+            System.out.println("Debut décryptage");
+            //Generer un objet Cipher
+            Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+            //Encrypter en utilisant la clé
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] decryptText = cipher.doFinal(crypText);
+            String result = new String(decryptText, "ISO-8859-1");
+            System.out.println("Message décrypté : ");
+            System.out.println(result);
+            System.out.println("Fin décryptage");
+            return result;  
+        } catch (Exception e){
+            System.out.println("Erreur décryptage");
+            return null;
+        }
+    }
 }
---
+++ src/test/java/h4414/ghome/camel/processors/SecurityTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package h4414.ghome.camel.processors;
+
+import java.security.Key;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Jérémy
+ */
+public class SecurityTest {
+    
+    public SecurityTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test d'encryption
+     */
+    @Test
+    public void testCryptage(){
+        Key key = Security.GenererCle();
+        String text = "Ceci est un texte décrypté!";
+        byte[] crypt = Security.Crypter(text, key);
+        String decrypt = Security.Decrypter(crypt, key);
+        Assert.assertEquals(text,decrypt);
+
+    }
+    
+}
