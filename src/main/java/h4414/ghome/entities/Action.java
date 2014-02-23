d6c27d5fae194271dff15dad4fbf623028fb9e35
mathis-paul <mathis.paul@insa-lyon.fr>
2014-02-24 00:45:54 +0100

    repare mes betises :)

--- src/main/java/h4414/ghome/entities/Action.java
+++ src/main/java/h4414/ghome/entities/Action.java
@@ -4,14 +4,37 @@
  */
 package h4414.ghome.entities;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
 
 
 /**
  *
  * @author Mathis
  */
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="actionType",discriminatorType=DiscriminatorType.STRING)
+@DiscriminatorValue("ACTION")
+public abstract class Action{
+    @Id
+    @GeneratedValue
+    private int id;
 
-public interface Action{
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
     
     
     
