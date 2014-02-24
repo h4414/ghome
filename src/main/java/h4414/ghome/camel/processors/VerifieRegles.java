2240c9210d72272b97f8f3e9ed31ba7b051e5b13
mathis-paul <mathis.paul@insa-lyon.fr>
2014-02-24 06:03:00 +0100

    module de verification des regles bien commence

--- src/main/java/h4414/ghome/camel/processors/VerifieRegles.java
+++ src/main/java/h4414/ghome/camel/processors/VerifieRegles.java
@@ -4,6 +4,9 @@
  */
 package h4414.ghome.camel.processors;
 
+import h4414.ghome.entities.Action;
+import h4414.ghome.entities.AllumerPrise;
+import h4414.ghome.entities.EnvoyerMail;
 import h4414.ghome.entities.Regle;
 import h4414.ghome.vues.PersistanceUtils;
 import java.util.ArrayList;
@@ -36,7 +39,31 @@ public class VerifieRegles implements Processor {
         if ( matchedRegles.isEmpty() ){
             //do nuthing
         }else{
-            
+            Iterator<Regle> mRegles= matchedRegles.iterator();
+            List<Action> actions = new ArrayList();
+            while ( mRegles.hasNext()){
+                actions.addAll(mRegles.next().getActions());
+            }
+            Iterator<Action> a = actions.iterator();
+            while ( a.hasNext()){
+                Action act = a.next();
+                ex.setProperty("listeEmail",new ArrayList<String>());
+                ex.setProperty("listePrises",new ArrayList<AllumerPrise>());
+                if ( act instanceof  EnvoyerMail){
+                    EnvoyerMail ac = (EnvoyerMail) act;
+                    ex.setProperty("email", true);
+                    List l = ex.getProperty("listeEmail",ArrayList.class);
+                    l.add(ac.getAdresse());
+                     ex.setProperty("listeEmail",l);
+                }
+                else if ( act instanceof AllumerPrise){
+                    AllumerPrise ac = (AllumerPrise) act;
+                    ex.setProperty("prise",true);
+                    List l = ex.getProperty("listePrises",ArrayList.class);
+                    l.add(ac);
+                     ex.setProperty("listePrises",l);
+                }
+            }
         }
         
     }
