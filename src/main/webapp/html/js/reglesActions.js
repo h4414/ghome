5e41c87ce129f756f4b61173b9a17c78637a6ae8
Jérémy <Jrmy@10.43.6.251>
2014-02-24 03:32:36 +0100

    ajout d'un select dans regleActions

--- src/main/webapp/html/js/reglesActions.js
+++ src/main/webapp/html/js/reglesActions.js
@@ -13,6 +13,26 @@ function init()
     var data = xmlHttp.responseText;
     var listeCapteur=JSON.parse(data);
     
+    var listePrise = new Array();
+    var j=0;
+    for (var i=0;i<listeCapteur.data.length;i++)
+    {
+        if (listeCapteur.data[i].type == "PRISE")
+        {
+            listePrise[j]=listeCapteur.data[i];
+            j++;
+        }
+    }
+    
+    for (var i=0;i<listePrise.length;i++)
+    {
+        var opt = document.createElement('option');
+        opt.value = listePrise[i].idCapteur;
+        opt.innerHTML = listePrise[i].nomCapteur;
+        select = document.getElementById('prise');
+        select.appendChild(opt);
+    }
+    
     
 }
 
